64af40b0f86f1f14dfd32e1ef32d596641c1bf63
Ruben Broekx <broekxruben@gmail.com>
2021-03-30 16:15:29 +0200

    Add defaults to tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,50 @@
 variable "package_slug" {
   description = "The package name as a slug in snake_case."
   type        = string
+  default     = "sentiment_flanders"
 }
 
 variable "package-slug" {
   description = "The package name as a slug in kebab-case."
   type        = string
+  default     = "sentiment_flanders"
 }
 
 variable "package_version" {
   description = "The package version."
   type        = string
+  default     = "0.0.0"
 }
 
 variable "git_branch" {
   description = "The branch this package version is based on."
   type        = string
+  default     = "master"
 }
 
 variable "organisation_account" {
   description = "The organisation account id."
   type        = string
+  default     = "840374773521"
 }
 
 variable "workspace_accounts" {
   description = "The account ids of the workspace accounts."
   type        = map(string)
+  default = {
+    "infrastructure" : "840374773521",
+    "feature" : "840374773521",
+    "development" : "840374773521",
+    "acceptance" : "840374773521",
+    "production" : "840374773521",
+    "default" : "840374773521"
+  }
 }
 
 variable "region" {
   description = "The cloud region to deploy to."
   type        = string
+  default     = "eu-west-1"
 }
 
 variable "deploy" {
